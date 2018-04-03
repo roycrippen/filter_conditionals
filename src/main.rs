51814244510c60daafaca5f9fd516cc34392fe1b
roycrippen <roycrippen@gmail.com>
2018-04-03 13:00:30 -0500

    command args

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+
 mod search;
 mod metrics;
 
-use search::{process_source, write_log};
-
-const SOURCE: &'static str = "/home/crippenre/dev/booster/OBV2_4_SC";
+use search::{get_file_list, process_source, write_log, Search};
 
 fn main() {
-    let metrics = process_source(SOURCE);
-    write_log(&metrics.log);
-    println!("{}", metrics.fmt_summary_metrics());
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+    let source = matches.value_of("INPUT").unwrap();
+
+    if matches.is_present("build") {
+        let search: Search = Default::default();
+        let files = get_file_list(source);
+        search.write_unique_target_ifs(&files);
+        println!("Unique target files built; inspect and modify them as necessary.");
+        println!("Files created:\n   affirmative_targets.txt\n   non_affirmative_targets.txt");
+    } else if matches.is_present("remove") {
+        let metrics = process_source(source);
+        write_log(&metrics.log);
+        println!("{}", metrics.fmt_summary_metrics());
+    } else {
+        println!("No valid option selected.\n-h for help");
+    }
 }
--- src/search.rs
+++ src/search.rs
@@ -3,12 +3,13 @@ extern crate walkdir;
 
 use self::walkdir::{DirEntry, WalkDir};
 use std::io::prelude::{Read, Write};
-use std::fs::File;
+use std::fs;
 use std::fmt::Display;
 use std::collections::HashSet;
 use std::fmt;
+use std::path::Path;
 
-use metrics::*;
+use metrics::{Mark, Metrics};
 
 pub struct Search {
     pub targets: HashSet<String>,
@@ -21,7 +22,16 @@ impl<'a> Search {
         self.targets.iter().any(|k| s.contains(k))
     }
 
-    pub fn debug_print_unique_target_ifs(&self, files: &Vec<(String, String)>) {
+    pub fn write_unique_target_ifs(&self, files: &Vec<(String, String)>) {
+        let s1 = "// This file is automatically created with the '-b' argument.";
+        let s2 = "// A backup is created with the extension '.backup'.";
+        let s3 = "// Inspect and modify this file manually before running with the -r argument.";
+        let insert_msg = |vs: &mut Vec<String>| {
+            vs.insert(0, s3.to_string());
+            vs.insert(0, s2.to_string());
+            vs.insert(0, s1.to_string());
+        };
+
         let mut xs = vec![];
         let mut ys = vec![];
         for &(ref fully_qualified_file, ref _file) in files {
@@ -39,19 +49,15 @@ impl<'a> Search {
 
         xs.sort();
         xs.dedup();
-        println!("Affirmative targets:");
-        for x in xs.iter() {
-            println!("{}", x.trim())
-        }
-        println!("");
+        insert_msg(&mut xs);
+        backup_file("affirmative_targets.txt", "backup");
+        write_file("affirmative_targets.txt", &xs);
 
         ys.sort();
         ys.dedup();
-        println!("Non-affirmative targets:");
-        for y in ys.iter() {
-            println!("{}", y.trim())
-        }
-        println!("")
+        insert_msg(&mut ys);
+        backup_file("non_affirmative_targets.txt", "backup");
+        write_file("non_affirmative_targets.txt", &ys);
     }
 }
 
@@ -103,66 +109,6 @@ impl<'a> fmt::Display for Search {
     }
 }
 
-pub fn process_source(source: &str) -> Metrics {
-    let search: Search = Default::default();
-    let mut metrics: Metrics = Default::default();
-    metrics.log.push(format!("{}", search));
-
-    let files = get_file_list(source);
-    metrics.total_files = files.len();
-
-    // search.debug_print_unique_target_ifs(&files);
-
-    for (full_file, _file) in files.clone() {
-        let mut mark: Mark = Default::default();
-        mark.file = full_file.to_string();
-
-        let mut ss = read_file(&full_file);
-        let mut start_line = 0;
-        let mut end_line = 0;
-        for (i, s) in ss.iter().enumerate() {
-            let process_lines =
-                |mark: &mut Mark, metrics: &mut Metrics, start_line: usize, end_line: usize| {
-                    mark.lines.push((start_line, end_line));
-                    metrics.log.push(format!("file:        {}", full_file));
-                    metrics.log.push(format!("Conditional: {}", s));
-                    let mut removed = add_line_nums_vec_range(&ss, start_line, end_line);
-                    metrics.log.append(&mut removed);
-                    metrics.log.push("\n".to_string());
-                };
-
-            if start_line > i || end_line > i {
-                continue;
-            }
-            if is_start(&s) {
-                if search.affirmative_targets.contains(s) {
-                    end_line = find_conditional_end_line(&ss, i, true);
-                    if end_line == 0 {
-                        continue;
-                    }
-                    process_lines(&mut mark, &mut metrics, i, end_line);
-                } else if search.non_affirmative_targets.contains(s) {
-                    start_line = find_else_start_line(&ss, i);
-                    if start_line == 0 {
-                        continue;
-                    }
-                    let end_line = find_conditional_end_line(&ss, start_line, false);
-                    if end_line == 0 {
-                        continue;
-                    }
-                    process_lines(&mut mark, &mut metrics, start_line, end_line);
-                }
-            }
-        }
-        if mark.lines.len() > 0 {
-            metrics.marks.push(mark);
-        }
-    }
-    let summary = metrics.fmt_summary_metrics();
-    metrics.log.push(summary.clone());
-    metrics
-}
-
 fn is_start(s: &str) -> bool {
     s.starts_with("#")
         && (s.starts_with("#if") || s.starts_with("#ifdef") || s.starts_with("#elif")
@@ -178,13 +124,19 @@ fn is_affirmative(s: &str) -> bool {
 }
 
 fn read_file(path: &str) -> Vec<String> {
-    let mut f = File::open(path).unwrap();
+    if !Path::new(path).exists() {
+        let xs = Vec::new();
+        write_file(path, &xs);
+        return xs;
+    }
+
+    let mut f = fs::File::open(path).unwrap();
     let mut buffer = String::new();
     f.read_to_string(&mut buffer).unwrap();
     buffer.lines().map(|s| s.to_string()).collect()
 }
 
-fn get_file_list(path: &str) -> Vec<(String, String)> {
+pub fn get_file_list(path: &str) -> Vec<(String, String)> {
     WalkDir::new(path)
         .into_iter()
         .map(|e| e.unwrap())
@@ -267,9 +219,15 @@ fn find_else_start_line(ss: &Vec<String>, start_line: usize) -> usize {
     0
 }
 
-pub fn write_log(log: &Vec<String>) {
-    let mut buffer = File::create("results.txt").unwrap();
-    log.iter().for_each(|s| write!(buffer, "{}\n", s).unwrap());
+fn write_file(file: &str, xs: &Vec<String>) {
+    let mut buffer = fs::File::create(file).unwrap();
+    xs.iter().for_each(|s| write!(buffer, "{}\n", s).unwrap());
+}
+
+fn backup_file(file: &str, extension: &str) {
+    if Path::new(file).exists() {
+        fs::copy(file, file.to_owned() + "." + extension).unwrap();
+    }
 }
 
 fn is_source(entry: &DirEntry) -> bool {
@@ -283,6 +241,12 @@ fn is_source(entry: &DirEntry) -> bool {
         .unwrap_or(false)
 }
 
+fn add_line_nums_vec_range<T: Display>(xs: &Vec<T>, start: usize, end: usize) -> Vec<String> {
+    let mut res: Vec<String> = Vec::new();
+    (start..end + 1).for_each(|i| res.push(format!("{:4} {}", i + 1, xs[i])));
+    res
+}
+
 pub fn print_vec<T: Display>(xs: &Vec<T>) {
     xs.iter().for_each(|x| println!("{}", x))
 }
@@ -291,8 +255,66 @@ pub fn print_vec_range<T: Display>(xs: &Vec<T>, start: usize, end: usize) {
     (start..end + 1).for_each(|i| println!("{:4} {}", i + 1, xs[i]))
 }
 
-fn add_line_nums_vec_range<T: Display>(xs: &Vec<T>, start: usize, end: usize) -> Vec<String> {
-    let mut res: Vec<String> = Vec::new();
-    (start..end + 1).for_each(|i| res.push(format!("{:4} {}", i + 1, xs[i])));
-    res
+pub fn write_log(log: &Vec<String>) {
+    write_file("results.txt", log)
+}
+
+pub fn process_source(source: &str) -> Metrics {
+    let search: Search = Default::default();
+    let mut metrics: Metrics = Default::default();
+    metrics.log.push(format!("{}", search));
+
+    let files = get_file_list(source);
+    metrics.total_files = files.len();
+
+    // search.debug_print_unique_target_ifs(&files);
+
+    for (full_file, _file) in files.clone() {
+        let mut mark: Mark = Default::default();
+        mark.file = full_file.to_string();
+
+        let mut ss = read_file(&full_file);
+        let mut start_line = 0;
+        let mut end_line = 0;
+        for (i, s) in ss.iter().enumerate() {
+            let process_lines =
+                |mark: &mut Mark, metrics: &mut Metrics, start_line: usize, end_line: usize| {
+                    mark.lines.push((start_line, end_line));
+                    metrics.log.push(format!("file:        {}", full_file));
+                    metrics.log.push(format!("Conditional: {}", s));
+                    let mut removed = add_line_nums_vec_range(&ss, start_line, end_line);
+                    metrics.log.append(&mut removed);
+                    metrics.log.push("\n".to_string());
+                };
+
+            if start_line > i || end_line > i {
+                continue;
+            }
+            if is_start(&s) {
+                if search.affirmative_targets.contains(s) {
+                    end_line = find_conditional_end_line(&ss, i, true);
+                    if end_line == 0 {
+                        continue;
+                    }
+                    process_lines(&mut mark, &mut metrics, i, end_line);
+                } else if search.non_affirmative_targets.contains(s) {
+                    start_line = find_else_start_line(&ss, i);
+                    if start_line == 0 {
+                        continue;
+                    }
+                    let end_line = find_conditional_end_line(&ss, start_line, false);
+                    if end_line == 0 {
+                        continue;
+                    }
+                    process_lines(&mut mark, &mut metrics, start_line, end_line);
+                }
+            }
+        }
+        if mark.lines.len() > 0 {
+            metrics.marks.push(mark);
+        }
+    }
+    let summary = metrics.fmt_summary_metrics();
+    metrics.log.push(summary.clone());
+    metrics
 }
